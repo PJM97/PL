fe5dd23d317db8889ebce7f7440d672b64c719b2
PJM97 <paulojorgecome@gmail.com>
2019-02-28 09:05:26 +0000

    teorica 28fev

---
+++ t_28fev/p1.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include <stdio.h>
+
+TEMPLATE a'''
+<H1>#x1</H1>
+bom "dia" #X2
+
+'''
+
+int main(){
+	a();
+	return 0;
+}
---
+++ t_28fev/prog.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+
+void a(){
+	printf("\n");
+	printf("<H1>#x1</H1>\n");
+	printf("bom \"dia\" #X2\n");
+	printf("\n");
+}
+
+int main(){
+	a();
+	return 0;
+}
